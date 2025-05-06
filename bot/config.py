942faef44387e701c3d9b0b95490d656053ddf37
pbonc <p.boncher@gmail.com>
2025-05-06 14:33:53 -0500

    bot

---
+++ bot/config.py
@@ -0,0 +1,6 @@
+import os
+
+# Twitch authentication and bot identity
+TWITCH_TOKEN = os.getenv("TWITCH_TOKEN")  # Example: 'oauth:abcd1234...'
+BOT_NICK = os.getenv("BOT_NICK", "botdar")  # Current bot username
+CHANNEL = os.getenv("CHANNEL", "iamdar")   # The Twitch channel to join
