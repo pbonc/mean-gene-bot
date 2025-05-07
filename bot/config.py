e555698ba64c8afa8d43a9d4ba9bea54e6b14399
pbonc <p.boncher@gmail.com>
2025-05-07 15:48:46 -0500

    botbot

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
 
-TWITCH_TOKEN = os.getenv("TWITCH_TOKEN")  # Should start with "oauth:"
-BOT_NICK = os.getenv("BOT_NICK", "meangenebot")  # Updated default
+load_dotenv()
+
+TWITCH_TOKEN = os.getenv("TWITCH_TOKEN")
+BOT_NICK = os.getenv("BOT_NICK", "meangenebot")
 CHANNEL = os.getenv("CHANNEL", "iamdar")
